5662d5021feba69bef5d39b4b4b62ae08cb8a59f
SKY8trash <127685370+SKY8trash@users.noreply.github.com>
2024-06-01 15:18:27 +0400

    Delete src/auth directory

--- src/auth/auth.router.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import authController from "./auth.controller.js";
-import { middlewareFunction } from "../middleware/middleware.js";
-export const authRouter = new Router();
-
-authRouter.use(middlewareFunction);
