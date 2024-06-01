06953b5c5f48a9b235ae554aacef9f6c5887ad81
SKY8trash <127685370+SKY8trash@users.noreply.github.com>
2024-06-01 15:19:02 +0400

    Add files via upload

---
+++ src/auth/auth.router.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import authController from "./auth.controller.js";
+import { middlewareFunction } from "../middleware/middleware.js";
+export const authRouter = new Router();
+
+authRouter.use(middlewareFunction);
+authRouter.post("/", authController.createAuth);
